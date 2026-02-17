bd1febc2e91241c558f646f4ed801e19eabda1b0
Kenneth A. Bettum Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2026-02-18 00:08:22 +0100

    Add environment variable template files
    Add example.env and firebaseEnv.template.js as templates for local environment configuration. Each file contains placeholder API keys and brief instructions to copy and rename (example.env -> .env, firebaseEnv.template.js -> firebaseEnv.js).

---
+++ firebaseEnv.template.js
@@ -0,0 +1,11 @@
+// Copy this file and rename it `firebaseEnv.js` and enter your API keys from Firebase Web App
+
+const firebaseConfig = {
+  apiKey: "XXX",
+  authDomain: "XXX",
+  projectId: "XXX",
+  storageBucket: "XXX",
+  messagingSenderId: "807998606067",
+  appId: "XXXX",
+};
+export default firebaseConfig;
