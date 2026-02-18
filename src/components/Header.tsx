12cb3e9042cf012f47941fae034f4562844aafd1
Kenneth A. Bettum Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2026-02-18 23:19:07 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,12 +7,9 @@ export default function Header() {
   const { top } = useSafeAreaInsets();
   return (
     <View style={{ paddingTop: top }}>
-      <View className="px-4 lg:px-6 h-14 flex items-center flex-row">
-        <Image
-          source="https://simpleicons.org/icons/expo.svg"
-          className="w-6 h-6 object-contain mr-2"
-        />
-        <Link className="font-bold flex-1 items-center justify-center" href="/">
+      <View className="px-4 lg:px-6 h-14 flex items-center flex-row bg-white border-b border-gray-200">
+        <Text className="font-bold text-lg mr-2">📦</Text>
+        <Link className="font-bold flex-1 text-lg" href="/">
           ACME
         </Link>
         <View className="flex flex-row gap-4 sm:gap-6">
