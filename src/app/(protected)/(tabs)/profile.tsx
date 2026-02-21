f34b16391618b111120e5d060863de784ae65be0
Kenneth Andre Bettum Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2026-02-21 15:32:20 +0100

    Added log out button to User Profile page

--- src/app/(protected)/(tabs)/profile.tsx
+++ src/app/(protected)/(tabs)/profile.tsx
@@ -9,20 +9,19 @@ export default function Index() {
   const { firebaseUser, userProfile } = useAuthContext();
 
   return (
-    <View className="flex-1 w-full h-full bg-white">
+    <View className="flex-1 w-full h-full p-10 bg-white items-center">
       <Text>Du er logget inn 👍 Dette er profilen din 😄</Text>
       {firebaseUser && (
-        <View className="mt-6">
+        <View className="mt-6 w-1/2 md:w-1/3">
           <Pressable
             className="bg-red-500 py-3 rounded-lg items-center"
             onPress={async () => {
               try {
                 await authApi.signOutUser();
-                showToast("success", "Logget ut!");
-                console.info(`User signed out.`);
               } catch (error: any) {
-                showToast("error", "Feil ved utlogging", error.message);
-                console.error(error?.message);
+                console.error(
+                  `🚨 ERROR in signing out user: ${error?.message} [Source: (tabs)/profile.tsx]`,
+                );
               }
             }}
           >
