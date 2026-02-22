3e32a2e2a84fdf0a0f3d68e4de3656c0e57b9e97
Kenneth Andre Bettum Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2026-02-22 17:41:12 +0100

    Added icons to Tabs menu from AntDesign

--- src/app/(protected)/(tabs)/_layout.tsx
+++ src/app/(protected)/(tabs)/_layout.tsx
@@ -1,12 +1,34 @@
 import React from "react";
 import { Tabs } from "expo-router";
 import Header from "@/components/Header";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+/*
+/ Denne koden er delvis basert på kodebasene fra forelesninger i faget TDS200 ved Høyskolen Kristiania høsten 2025.
+/ Brukt med tillatelse.
+*/
 
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{ headerShown: true, header: () => <Header /> }}>
-      <Tabs.Screen name="home" options={{ title: "Hjem" }} />
-      <Tabs.Screen name="profile" options={{ title: "Min profil" }} />
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Hjem",
+          tabBarIcon: ({ color, focused }) => (
+            <AntDesign name="home" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Min profil",
+          tabBarIcon: ({ color, focused }) => (
+            <AntDesign name="idcard" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
