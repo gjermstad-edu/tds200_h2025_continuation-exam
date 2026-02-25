2032a9470a935917ef71a87a058480d2f39ba34d
Kenneth Andre Bettum Gjermstad <79542593+Gjermstad@users.noreply.github.com>
2026-02-25 14:23:20 +0100

    Added eye-icon to show/hide password text

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { View, Text, TextInput, TextInputProps } from "react-native";
+import React, { useState } from "react";
+import { View, Text, TextInput, TextInputProps, Pressable } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 type InputFieldProps = TextInputProps & {
   label: string;
@@ -15,6 +16,9 @@ export function FormInput({
   password = false,
   ...props
 }: InputFieldProps) {
+  const [isPasswordHidden, setIsPasswordHidden] = useState(true);
+
+  // Setter farge på kanten ut ifra om godkjent state utenfra
   const borderClassName =
     valid === true
       ? "border-green-500"
@@ -22,17 +26,35 @@ export function FormInput({
         ? "border-red-500"
         : "border-gray-300";
 
-  const isPasswordField = password === true;
+  // Om tekstfeltet er et passord + at det skal være skjult pga øyet
+  const isPasswordField = password ? isPasswordHidden : false;
 
   return (
     <View className="mt-4">
       <Text className="text-gray-800 mb-1">{label}</Text>
 
-      <TextInput
-        {...props}
-        secureTextEntry={isPasswordField}
-        className={`border rounded-lg px-3 py-2 ${borderClassName}`}
-      />
+      <View
+        className={`flex-row items-center border rounded-lg ${borderClassName}`}
+      >
+        <TextInput
+          {...props}
+          secureTextEntry={isPasswordField}
+          autoCapitalize="none"
+          className="flex-1 px-3 py-2 focus:outline-none focus:ring-0 outline-none"
+        />
+        {password && (
+          <Pressable
+            onPress={() => setIsPasswordHidden((prev) => !prev)}
+            className="px-3"
+          >
+            <Ionicons
+              name={isPasswordHidden ? "eye-off-outline" : "eye-outline"}
+              size={20}
+              color="#6B7280"
+            />
+          </Pressable>
+        )}
+      </View>
 
       {!!error && <Text className="text-red-600 text-sm mt-1">{error}</Text>}
     </View>
